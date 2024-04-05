60a2b08fe56dc9d621cf9529cbeb221d85f7e3d1
christofelician <163845413+christofelician@users.noreply.github.com>
2024-04-05 12:14:20 +0200

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,8 +1,6 @@
-
 from utils import flaecheRechteck
 
 def test_flaecheRechteck():
     assert flaecheRechteck(3,4) == 12
     assert flaecheRechteck(10,10) == 100
     assert flaecheRechteck(4,3) == 12
-
