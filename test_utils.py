305640adc11b06a7e770f621bcfd5c3a5a1599f3
christofelician <lemberger.christoph@gmail.com>
2024-04-05 12:19:49 +0200

    strange fail

--- test_utils.py
+++ test_utils.py
@@ -1,17 +1,7 @@
 from utils import flaecheRechteck
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def test_flächeRechteckBerechenen():
-    assert flächeRechteckBerechenen(3,4) == 12
-    assert flächeRechteckBerechenen(10,10) == 100
-    assert flächeRechteckBerechenen(4,3) == 12
-=======
 
-=======
->>>>>>> 20da046974bb409fe64994e95d43e3c66987ddda
-def test_flaecheRechteck():
+def test_flächeRechteckBerechenen():
     assert flaecheRechteck(3,4) == 12
     assert flaecheRechteck(10,10) == 100
     assert flaecheRechteck(4,3) == 12
->>>>>>> 2a46bffe0d07b242821e5d359abb2f0dc4a5c92a
