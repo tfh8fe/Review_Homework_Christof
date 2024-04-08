aa0a43ef3c19b9452808ace450c85e8c324f4e98
christofelician <lemberger.christoph@gmail.com>
2024-04-08 17:31:49 +0200

    testing friend

--- utils2.py
+++ utils2.py
@@ -1,6 +1,6 @@
 def calc_crf(int_rate, lifetime):
     """Calculation of capital ercovery factor"""
-    crf = ((1 + int_rate) ** lifetime * int_rate) / ((1 + int_rate) ** lifetime - 1)
+    crf = ((1 + int_rate) * lifetime * int_rate) / ((1 + int_rate) * lifetime - 1)
     return crf
 
 
