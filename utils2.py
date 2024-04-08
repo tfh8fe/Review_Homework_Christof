757922aaf1b1e675d761223cbef00ff7d4a9eaab
christofelician <lemberger.christoph@gmail.com>
2024-04-08 17:30:43 +0200

    testing for friensd

--- utils2.py
+++ utils2.py
@@ -1,9 +1,3 @@
-"""
-This module defines the functions required for calculating net present value.
-The parameters are defined in main.py
-"""
-
-
 def calc_crf(int_rate, lifetime):
     """Calculation of capital ercovery factor"""
     crf = ((1 + int_rate) ** lifetime * int_rate) / ((1 + int_rate) ** lifetime - 1)
@@ -11,8 +5,7 @@ def calc_crf(int_rate, lifetime):
 
 
 def calc_self_consumption(installed_capacity, full_load_hours):
-    """Calculation of self-consumption per yeaar with the assumption that
-    50% of the energy generated is consumed"""
+    """Calculation of self-consumption per yeaar with the assumption that 50% of the energy generated is consumed"""
     self_consumption = installed_capacity * full_load_hours * 0.5
     return self_consumption
 
@@ -32,7 +25,6 @@ def calc_annual_revenues(self_consumption, feedin_tarif):
 def calc_npv(
     investment_costs, installed_capacity, crf, annual_costs, annual_revenues):
     """Calculation of net present value over the lifetime"""
-
     npv = [0]
     npv[0] = -(investment_costs*installed_capacity)
     for i in range(1, 20):
