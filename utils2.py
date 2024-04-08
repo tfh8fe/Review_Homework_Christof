96f6aeb9eb397d256634c975b617c0a923fe6484
christofelician <lemberger.christoph@gmail.com>
2024-04-08 17:51:41 +0200

    asd

--- utils2.py
+++ utils2.py
@@ -5,12 +5,6 @@ def calc_self_consumption(installed_capacity, full_load_hours):
     return self_consumption
 
 
-def calc_annual_costs(annual_load, self_consumption, supply_tarif):
-    """Calculation of annual electricity costs"""
-    annual_costs = (annual_load-self_consumption) * supply_tarif
-    return annual_costs
-
-
 def calc_annual_revenues(self_consumption, feedin_tarif):
     """Calculation of annual revenues"""
     annual_revenues = self_consumption * feedin_tarif
